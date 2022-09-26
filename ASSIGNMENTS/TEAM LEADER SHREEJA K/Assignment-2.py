bb5d78960b668319edaffcb34d68d16225157969
Shreejashree <113234510+Shreejashree@users.noreply.github.com>
2022-09-26 12:10:17 +0530

    Add files via upload

---
+++ ASSIGNMENTS/TEAM LEADER SHREEJA K/Assignment-2.py
@@ -0,0 +1,23 @@
+'''Temperature and Humidity is gernerated Randomly,if it is above the required temperature
+and Humidity then an alarm is set to detect and gives message as high temperature and more
+Humidity is detected'''
+
+
+import random
+import time
+
+while(1):
+    time.sleep(2) #2 seconds delay
+    temperature = random.randrange(0,100)
+    humidity = random.randrange(0,100)
+    
+    print("temperature=",temperature)
+    print("humdity=",humidity)
+    
+    if(temperature>40 or humidity>60):
+        if(temperature>40 and humidity>60):   #Detection of High temperature and humdity
+            print("High temperature and More humdity Detected")
+        elif(humidity>60 and temperature<40): #Detection of High humdity
+            print("More humdity Detected")
+        else:                                 #Detection of High temperature
+            print("High temperature Detected")
